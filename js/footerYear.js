74e943295ff8c858206cafc59c02a00192a1401b
MadanAmpera <madanampera@hotmail.com>
2025-01-18 22:56:25 +1030

    All mobile designs - partial footer year js

---
+++ js/footerYear.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const yearSpan = document.getElementById('year');
+    if (yearSpan) {
+        const currentYear = new Date().getFullYear();
+        yearSpan.textContent = currentYear;
+    }
+});
